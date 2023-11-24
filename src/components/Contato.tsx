a7542309943d099f69c4dbf115828f86b2874749
devruso <jamilsonjr@live.com>
2023-11-24 01:37:20 -0300

    feat: Add contato component

---
+++ src/components/Contato.tsx
@@ -0,0 +1,16 @@
+import bg from "../assets/bg-2.png";
+
+export default function Contato(): JSX.Element {
+  return (
+    <section
+      className="w-full bg-textColor bg-cover bg-center flex flex-col  py-16"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="max-w-3xl text-white text-left ml-96">
+        <h1 className="text-5xl font-bold mb-4">Entre em Contato</h1>
+        <p className="mb-4 text-lg">Telefone e Whatsapp: 71 99723-0152 / 71 99662-60059</p>
+        <p className="mb-8 text-lg">E-mail: contato@2fc.com.br</p>
+      </div>
+    </section>
+  ); 
+}
