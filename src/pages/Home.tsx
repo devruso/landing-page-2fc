19e2e977165ce43a6e2d0556b9a5ea7d1564cd6c
devruso <jamilsonjr@live.com>
2023-12-13 09:25:52 -0300

    refactor: change component organization

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import AreasAtuacao from "../components/AreasAtuacao";
 import Contato from "../components/Contato";
-import QuemSomos from "../components/QuemSomos";
+import HeroSection from "../components/HeroSection";
 import Solucoes from "../components/Solucoes";
 
 export default function Home(): JSX.Element{
     return (
         < >
-            <QuemSomos/>
+            <HeroSection/>
             <Solucoes />
             <AreasAtuacao />
             <Contato />
