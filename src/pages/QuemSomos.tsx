57f922081dcbd2fc57d93d8cf06cc4b8cefb9b05
devruso <jamilsonjr@live.com>
2023-12-13 09:25:03 -0300

    feat/wip: who we are

---
+++ src/pages/QuemSomos.tsx
@@ -0,0 +1,11 @@
+export default function QuemSomos():JSX.Element{
+
+return(
+    <div className="max-w-4xl bg-gray-400 px-4  bg-opacity-40 rounded-md mx-auto text-lg text-textColor">
+
+    <p className="mb-4 mt-16 leading-relaxed ">
+    A 2FC Gestão Contábil traz na bagagem gestores com experiência acumulada de mais de 15 anos na área contábil, com passagem por São Paulo, porém com vasto background no escritório tradicional da família. Contamos com uma equipe altamente qualificada para te atender de forma profissional e ética, aliando a tecnologia às melhores soluções para seu negócio.
+      </p>
+
+    </div>
+    );}
