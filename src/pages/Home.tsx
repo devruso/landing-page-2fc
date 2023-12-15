877e82e3a74e81ae0e3cdf97f5f1de9133c512c6
devruso <jamilsonjr@live.com>
2023-12-15 17:30:04 -0300

    feat: adds layout to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
  import Contato from "../components/Contato";
 import HeroSection from "../components/HeroSection";
 import Solucoes from "../components/Solucoes";
+import Layout from "../components/Layout";
+
 export default function Home(): JSX.Element{
     return (
         < >
+            <Layout/>
             <HeroSection/>
             <Solucoes />
             <Contato />
