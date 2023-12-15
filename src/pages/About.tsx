d879722bb258867b8ca36457f884789974b11517
devruso <jamilsonjr@live.com>
2023-12-15 15:48:33 -0300

    fix: adds horizontal padding

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ export default function About():JSX.Element{
     
     <div className="max-w-4xl px-4 flex flex-col bg-opacity-40 rounded-md mx-auto my-20 text-lg text-vinho">
         <h1 className="text-5xl self-center">Sobre nós</h1>
-        <p className="mb-4 text-2xl mt-16 leading-relaxed text-justify md:mb-40">
+        <p className="mb-4 text-2xl mt-16 leading-relaxed text-justify md:mb-40 px-2">
         A 2FC Gestão Contábil agrega experiência e conhecimento de executivos com mais de 15 anos de atuação na área, passagem por grandes centros de negócios e vasto background no escritório tradicional da família. Contamos com time de profissionais altamente qualificados e comprometidos com a segurança do correto cumprimento das obrigações, bem como apoio na tomada de decisão, apresentando requisitos de governança, ética e responsabilidade social corporativa.
         </p>
 
