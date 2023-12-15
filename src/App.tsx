b8ee366e9315c69a9194abed853ff0f92acfdd1e
devruso <jamilsonjr@live.com>
2023-12-15 14:57:25 -0300

    feat: adds about  page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Root from './pages/Root';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes >
       <Route path="/" element={<Root />} >
-      <Route index element={<Home/>}/>  
+      <Route index element={<Home/>}/> 
+      <Route path="/sobre" element={<About/>}/> 
       </Route>
     </Routes>
     </BrowserRouter>
---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+export default function About():JSX.Element{
+    return(    
+    
+    <div className="max-w-4xl px-4 flex flex-col bg-opacity-40 rounded-md mx-auto my-20 text-lg text-vinho">
+        <h1 className="text-5xl self-center">Sobre nós</h1>
+        <p className="mb-4 text-2xl mt-16 leading-relaxed text-justify md:mb-40">
+        A 2FC Gestão Contábil agrega experiência e conhecimento de executivos com mais de 15 anos de atuação na área, passagem por grandes centros de negócios e vasto background no escritório tradicional da família. Contamos com time de profissionais altamente qualificados e comprometidos com a segurança do correto cumprimento das obrigações, bem como apoio na tomada de decisão, apresentando requisitos de governança, ética e responsabilidade social corporativa.
+        </p>
+
+    </div>)
+}
