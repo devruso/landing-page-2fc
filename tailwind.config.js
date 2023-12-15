9ef04594ce7bd17bf5957c81d68c5579d85038fb
devruso <jamilsonjr@live.com>
2023-12-15 15:47:53 -0300

    build: add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         textColor: "#212733",
         pistache: "#DDD2C0",
         vinho:  "#390F16",
-        vinho2: "#631F2A"
+        vinho2: "#631F2A",
+        vinho3: "#491017",
       }
     },
   },
