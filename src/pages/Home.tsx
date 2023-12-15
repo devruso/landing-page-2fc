96abc9ae16da557c37639a3272e86a91331c9e0f
devruso <jamilsonjr@live.com>
2023-12-14 23:13:35 -0300

    refactor: remove unused component & adds footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,14 @@
-import AreasAtuacao from "../components/AreasAtuacao";
 import Contato from "../components/Contato";
 import HeroSection from "../components/HeroSection";
 import Solucoes from "../components/Solucoes";
-
+import Footer from "../components/Footer";
 export default function Home(): JSX.Element{
     return (
         < >
             <HeroSection/>
             <Solucoes />
-            <AreasAtuacao />
             <Contato />
+            <Footer/>
         </>
     );
 }
