24169c7d201224eaf924105f06fb76c2aed91957
devruso <jamilsonjr@live.com>
2023-12-15 14:58:06 -0300

    fix: corrects footer location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
-import Contato from "../components/Contato";
+ import Contato from "../components/Contato";
 import HeroSection from "../components/HeroSection";
 import Solucoes from "../components/Solucoes";
-import Footer from "../components/Footer";
 export default function Home(): JSX.Element{
     return (
         < >
             <HeroSection/>
             <Solucoes />
             <Contato />
-            <Footer/>
         </>
     );
 }
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-
+import Footer from "../components/Footer";
 export default function Root() {
     return (
       <>
         <Header />
         <Outlet />
-        {/* <Footer /> */}
+        <Footer /> 
       </>
     );
   }
