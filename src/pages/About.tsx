7a35147ce7f81c735e8daa2af6632e494decb5fc
devruso <jamilsonjr@live.com>
2023-12-15 19:37:35 -0300

    fix: letter spacing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,12 @@ export default function About():JSX.Element{
     
     <div className="max-w-4xl px-4 flex flex-col bg-opacity-40 rounded-md mx-auto my-20 text-lg text-vinho">
         <h1 className="text-5xl self-center">Sobre nós</h1>
-        <p className="mb-4 text-2xl mt-16 leading-relaxed text-justify md:mb-40 px-2">
-        A 2FC Gestão Contábil agrega experiência e conhecimento de executivos com mais de 15 anos de atuação na área, passagem por grandes centros de negócios e vasto background no escritório tradicional da família. Contamos com time de profissionais altamente qualificados e comprometidos com a segurança do correto cumprimento das obrigações, bem como apoio na tomada de decisão, apresentando requisitos de governança, ética e responsabilidade social corporativa.
+        <p className="mb-4 text-2xl mt-16 leading-normal  md:tracking-wide md:leading-relaxed text-justify md:mb-40 px-2">
+        A <p className="inline text-3xl font-light">2</p>FC Gestão Contábil agrega experiência e conhecimento de executivos com mais
+         de 15 anos de atuação na área, passagem por grandes centros de negócios e vasto 
+         background no escritório tradicional da família. Contamos com time de profissionais altamente qualificados
+          e comprometidos com a segurança do correto cumprimento das obrigações, bem como apoio na tomada de decisão, 
+          apresentando requisitos de governança, ética e responsabilidade social corporativa.
         </p>
 
     </div>)
